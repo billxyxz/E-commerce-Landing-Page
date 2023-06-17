67e58dc434e233574701e16c7b10e416597ba4f2
Collins Erobaga <erobagacollins@gmail.com>
2023-06-17 23:59:22 +0100

    worked on the responsiveness and tried some gsap animation

--- public/menu.js
+++ public/menu.js
@@ -8,3 +8,26 @@ hamburger.addEventListener("click", () => {
 navLinks.addEventListener("click", () =>{
     navLinks.classList.add("translate-x-full");
 })
+
+gsap.registerPlugin(ScrollTrigger)
+
+const tl = gsap.timeline();
+tl
+.from(".hero-img", {xPercent: 100, opacity: 0, duration: 1, scale: .7})
+.from(".hero-text", {opacity: 0, duration: 1}, "-=.5")
+
+gsap.from( ".item", 
+    {
+     scrollTrigger: {
+        trigger: "#shop",
+        start: "top 80%",
+        end: "bottom 30%",
+        toggleActions: "restart reverse restart restart"
+     },
+    opacity: 0,
+    duration: 1, 
+    ease: "power1.out",
+    stagger: .5
+ }
+
+)
